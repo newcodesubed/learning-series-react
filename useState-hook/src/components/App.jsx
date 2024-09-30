4975152933db4c8f5457496cfab233bcf6fd4f32
Subed <subedshahxi@gmail.com>
2024-09-30 16:58:04 +0545

    feat : use of useState

--- useState-hook/src/components/App.jsx
+++ useState-hook/src/components/App.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return <div />;
+  const [count, setCount] =useState(0);
+
+  function increment(){
+    setCount(count + 1);
+  }
+  function decrement(){
+    setCount(count - 1);
+  }
+  return (
+    <div>
+      <div className="container">
+    <h1>{count}</h1>
+    <button onClick={increment}>+</button>
+    <button onClick={decrement}>-</button>
+    </div>
+    </div>
+  );
 }
 
 export default App;
