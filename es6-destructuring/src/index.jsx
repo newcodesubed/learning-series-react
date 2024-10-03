29360eefb2348f02a4e74b0712065a7e2e192d32
Subed <subedshahxi@gmail.com>
2024-10-03 10:13:06 +0545

    feat : data destructuring

--- es6-destructuring/src/index.jsx
+++ es6-destructuring/src/index.jsx
@@ -1,29 +1,42 @@
-import animals from "./data";
+// import animals from "./data";
 
-console.log(animals);
+// console.log(animals);
+// //destructuring array
+// const [cat, dog]= animals;
+
+// console.log(cat);
+
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
+const [honda, tesla] = cars;
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+const {speedStats : {topSpeed:hondaTopSpeed}} = honda;
+const {speedStats : {topSpeed:teslaTopSpeed}} = tesla;
+
+const {coloursByPopularity : [teslaTopColour]} = tesla;
+const {coloursByPopularity : [hondaTopColour]} = honda;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
 
 // If you're running this locally in VS Code use the commands:
 // npm install
