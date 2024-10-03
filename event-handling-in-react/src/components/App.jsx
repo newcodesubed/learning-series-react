86500fedf1f4558f59adfe03599f97cc29e05dfc
Subed <subedshahxi@gmail.com>
2024-10-03 20:06:32 +0545

    feat : onMouseOver/Out

--- event-handling-in-react/src/components/App.jsx
+++ event-handling-in-react/src/components/App.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText]= useState("HELLO");
+  const [mouseOver, setMouseOver]= useState(false);
+  function clickHandler(){
+    setHeadingText("submitted");
+    
+  }
+  function hoverHandler(){
+    setMouseOver(true);
+    
+  }
+  function mouseOutHandler(){
+    setMouseOver(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button style={{backgroundColor:mouseOver ? "black" : "white"}} 
+      onClick={clickHandler} 
+      onMouseOver={hoverHandler}
+      onMouseOut={mouseOutHandler}>Submit</button>
     </div>
   );
 }
