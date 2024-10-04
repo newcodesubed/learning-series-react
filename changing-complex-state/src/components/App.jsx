df35700c102bd4f76019fd097dadca46eff7752d
Subed <subedshahxi@gmail.com>
2024-10-04 15:38:26 +0545

    update : useState

--- changing-complex-state/src/components/App.jsx
+++ changing-complex-state/src/components/App.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const[fName,setFName]=useState("");
+  const[lName,setLName]=useState("")
+
+  function handleChangeF(event){
+    setFName(event.target.value);
+  }
+  function handleChangeL(event){
+    setLName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fName} {lName} </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input 
+          name="fName"
+          placeholder="First Name" 
+          onChange={handleChangeF}
+          value={fName}
+          />
+        <input name="lName" 
+        placeholder="Last Name" 
+        onChange={handleChangeL}
+        value={lName}/>
+        
+
         <button>Submit</button>
       </form>
     </div>
