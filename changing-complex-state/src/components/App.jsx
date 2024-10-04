13ffba8114f82c9cda723c1195cf8327429491e8
Subed <subedshahxi@gmail.com>
2024-10-04 15:57:42 +0545

    refactor : form field

--- changing-complex-state/src/components/App.jsx
+++ changing-complex-state/src/components/App.jsx
@@ -1,30 +1,48 @@
 import React, { useState } from "react";
 
 function App() {
-  const[fName,setFName]=useState("");
-  const[lName,setLName]=useState("")
+  // const[fName,setFName]=useState("");
+  // const[lName,setLName]=useState("");
+  const [fullName, setFullName]=useState({fName:"",lName:""});
 
-  function handleChangeF(event){
-    setFName(event.target.value);
+  function handleChange(event){
+    // const newValue = event.target.value;
+    // const kMaHalekoName = event.target.name;
+//sano banauna milxa with destructuring 
+    const {value, name} = event.target;
+
+    setFullName(prevValue =>{
+      if (name === "fName"){
+        return {
+          fName:value,
+          lName:prevValue.lName
+        };
+      }else if (name === "lName"){
+       //object bayer format ni object ko xa
+        return {
+          fName:prevValue.fName,
+          lName:value
+        };
+      }
+    });
   }
-  function handleChangeL(event){
-    setLName(event.target.value);
-  }
+  
 
   return (
     <div className="container">
-      <h1>Hello {fName} {lName} </h1>
+      <h1>Hello {fullName.fName} {fullName.lName} </h1>
       <form>
         <input 
           name="fName"
           placeholder="First Name" 
-          onChange={handleChangeF}
-          value={fName}
+          onChange={handleChange}
+          value={fullName.fName}
           />
         <input name="lName" 
         placeholder="Last Name" 
-        onChange={handleChangeL}
-        value={lName}/>
+        onChange={handleChange}
+        value={fullName.lName}
+        />
         
 
         <button>Submit</button>
